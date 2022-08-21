44643bdf96f546df39abb2d034ea045cfa6a5bb3
Iban-blade <2734319366@qq.com>
2022-08-22 00:14:47 +0800

    add

--- cloud-service-demo/src/main/java/com/ivan/controller/HelloController.java
+++ cloud-service-demo/src/main/java/com/ivan/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.ivan.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
